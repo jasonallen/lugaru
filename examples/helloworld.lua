4ac768e20c1c309043373c42c660335414e01bfe
Jason Allen <jasonallen@gmail.com>
2013-07-17 17:14:08 -0700

    cleaning up helloworld example

--- examples/helloworld.lua
+++ examples/helloworld.lua
@@ -1,14 +1,13 @@
 package.path = package.path .. ";src/?.lua"
 
 local L = require 'lugaru'
-local Position = require 'components.position'
 
 L.init(320, 480)
 
 local setup = function(scene)
   local e = L.Entity()
   e:addComponent(L.Position(0,0))
-  e:addComponent(L.Sprite("examples/foo.png", 64, 64))
+  e:addComponent(L.Sprite("examples/foo.png", 128, 128))
   scene:addEntity(e)
 end
 
